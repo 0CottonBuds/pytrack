e9528bb109dd8920ab4a47860c738fe0ccfee570
unknown <emiljohnllanes772@gmail.com>
2024-08-04 11:37:47 +0800

    rename WindowTime to WindowTimeElapsed, rename some of its methods, update comments

--- PytrackLibs/window.py
+++ PytrackLibs/window.py
@@ -2,7 +2,7 @@ from Helpers.database_helper import find_window_on_database_by_name
 
 
 class Window:
-    """class to handle and store data that is retrieved from the database"""
+    """"""
 
     short_name: str = ""
     name: str = ""
@@ -20,17 +20,16 @@ class Window:
 
         self.short_name: str = split_window_name[-1]
         self.name: str = window_name
-        self.time_elapsed = WindowTime(time_elapsed)
+        self.time_elapsed = WindowTimeElapsed(time_elapsed)
         self.date_entered = date_entered
 
     def __str__(self) -> str:
         return f"name: {self.name}\ntype: {self.type}\nrating: {self.rating}"
 
-class WindowTime:
-    """class window time for keeping data(time)"""
-
-    name: str
-    full_name: str
+class WindowTimeElapsed:
+    hours: float
+    minutes: float
+    seconds: float
     percentage: float
 
     def __init__(self, time_elapsed: str) -> None:
@@ -40,42 +39,42 @@ class WindowTime:
         self.minutes: float = float(split_time_elapsed[1])
         self.seconds: float = float(split_time_elapsed[2])
 
-        self.format_time()
+        self.handle_negative()
 
-    def get_time(self) -> tuple:
-        """returns tuple of time"""
-        self.format_time()
-        time = (self.hours, self.minutes, self.seconds)
-        return time
-
-    def format_time(self):
-        """formats the time to avoid the negatives and time going above 60 \n use this when changing the time value"""
-        if self.minutes > 60:
+    def to_tuple(self) -> tuple:
+        self.handle_negative()
+        return (self.hours, self.minutes, self.seconds)    
+    
+    def handle_negative(self):
+        """handles the negatives values and values going above their respective limit(24, 60, 60)"""
+        while self.minutes > 60:
             self.minutes -= 60
             self.hours += 1
-        if self.seconds > 60:
+        while self.seconds > 60:
             self.seconds -= 60
             self.minutes += 1
-        if self.seconds < 0:
+        while self.seconds < 0:
             self.seconds += 60
-        if self.minutes < 0:
+            self.minutes -= 1
+        while self.minutes < 0:
             self.minutes += 60
+            self.hours -= 1
 
-    def __add__(self, other: "WindowTime") -> "WindowTime":
+    def __add__(self, other: "WindowTimeElapsed") -> "WindowTimeElapsed":
         self.hours = self.hours + other.hours
         self.minutes = self.minutes + other.minutes
         self.seconds = self.seconds + other.seconds
 
-        self.format_time()
+        self.handle_negative()
 
-        return WindowTime(f"{self.hours}, {self.minutes}, {self.seconds}")
+        return WindowTimeElapsed(f"{self.hours}, {self.minutes}, {self.seconds}")
 
-def check_app_type(window_title : str):
-    '''Takes a window checks and assign values to the instance."'''
+def get_window_by_name(window_name : str) -> Window:
 
-    separated_window_title = window_title.split("- ")
+    # we do this because pygetwindow gives window names like operagx - youtube.com
+    separated_window_name = window_name.split("- ")
 
-    for slice in separated_window_title:
+    for slice in separated_window_name:
         window = find_window_on_database_by_name(slice)
 
         if window != None:
